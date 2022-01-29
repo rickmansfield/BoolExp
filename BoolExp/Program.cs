72a3507eebe59c2f9b34c4ba1a50acef41eb34c4
Rick <ricksmycodeguy@gmail.com>
2022-01-29 17:00:30 -0600

    Add project files.

---
+++ BoolExp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BoolExp
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      int apples = 18;
+      int oranges = 9;
+      Console.WriteLine(apples == oranges);
+      Console.WriteLine(apples != oranges);
+      Console.WriteLine($"{apples} > {oranges} is {apples > oranges}");
+      Console.WriteLine($"{apples} < {oranges} is {apples < oranges}");
+      Console.WriteLine($"{apples} >= {oranges} is {apples >= oranges}");
+      Console.WriteLine($"{apples} <= {oranges} is {apples <= oranges}");
+      Console.WriteLine($"{apples} is int is {apples is int}");
+        }
+  }
+}
